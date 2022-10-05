174bb4056777378f94c45d86692ad262b0925201
154147J <154147j@uom.lk>
2022-10-05 16:59:24 +0530

    Hello i commited this

---
+++ hello.js
@@ -0,0 +1,5 @@
+var x = ['qq', 'www', 'xx', 'cc', 'gg', 'bb'];
+var y = ['tt', 'yy', 'uu', 'mm', 'zz'];
+console.log(x.concat(y));
+console.log(y);
+console.log(x);
---
+++ hello.ts
@@ -0,0 +1,11 @@
+let x = ['qq','www','xx','cc','gg','bb']
+let y = ['tt','yy','uu','mm','zz']
+
+console.log(x.concat(y));
+console.log(y);
+
+
+console.log(x);
+
+
+
